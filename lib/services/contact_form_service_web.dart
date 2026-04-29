84c9861393e04850da6172089f06db9d45af91f7
sirnaet <bsmemusi@gmail.com>
2026-04-29 19:30:40 +0300

    Minor change

--- lib/services/contact_form_service_web.dart
+++ lib/services/contact_form_service_web.dart
@@ -5,7 +5,7 @@ import 'dart:html' as html;
 
 class ContactFormService {
   static const _defaultEndpoint =
-      'https://script.google.com/macros/s/AKfycbxUhN6EBrjzBQvzRrfifFaCF4IJ8O3oTxilxxAiIon_nQDIMVDBtEv6niNDQS_jLcqK/exec';
+      'https://script.google.com/macros/s/AKfycbx1OqZD-29bsy0nI3KqzK9ScOgoctTSsIl1oXZkY3wDPZccVAxjhgUuXgicbKbBx7N2/exec';
   static const _endpoint = String.fromEnvironment(
     'CONTACT_FORM_ENDPOINT',
     defaultValue: _defaultEndpoint,
