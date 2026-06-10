d63f319d3dc67d91fea8cc42dabeae9a460fb137
sirnaet <bsmemusi@gmail.com>
2026-06-10 19:45:17 +0300

    Website Cleanup

--- react-portfolio/vite.config.ts
+++ react-portfolio/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/react-portfolio/',
   build: {
+    outDir: '../web/react-portfolio',
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/index.js',
